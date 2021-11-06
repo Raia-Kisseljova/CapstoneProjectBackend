6fa088cebea5308238c65139ec5ce6db8c6c05d4
Raia Kisseljova <84378854+Raissa-Kisseljova@users.noreply.github.com>
2021-11-06 15:04:48 +0000

    feat: router to check if email,name and nicknames are not used

--- src/Routers/userRouter.ts
+++ src/Routers/userRouter.ts
@@ -1,18 +1,73 @@
 import express from "express";
-import User from "../schemas/userSchema";
 import auth from "../middleware/auth";
+import basicUser from "../models/basicUser";
+import Organisation from "../models/organisation";
+import User from "../schemas/userSchema";
 
 const userRouter = express.Router();
 
 //Find user by nickname
 
-userRouter.get("/:nickname", async (req, res, next) => {
+// userRouter.get("/:_id", async (req, res, next) => {
+//   console.log(req.params);
+//   try {
+//     const user = await User.findById({
+//       _id: req.params._id,
+//     });
+//     console.log("user is", user);
+//     if (!user) {
+//       res.status(404).send({
+//         message: `User with _id: '${req.params._id}' was not found!`,
+//       });
+//     } else res.send(user);
+//   } catch (error) {
+//     next();
+//   }
+// });\
+
+userRouter.get("/is_unique", async (req, res, next) => {
+  if (req.query.name) {
+    try {
+      const user = await Organisation.findOne({
+        name: req.query.name as string,
+      });
+      res.send({ name: user ? false : true });
+    } catch (error) {
+      next();
+    }
+  }
+
+  if (req.query.nickname) {
+    try {
+      const user = await basicUser.findOne({
+        nickname: req.query.nickname as string,
+      });
+      res.send({ nickname: user ? false : true });
+    } catch (error) {
+      next();
+    }
+  }
+
+  if (req.query.email) {
+    try {
+      const user = await User.findOne({ email: req.query.email as string });
+      res.send({ email: user ? false : true });
+    } catch (error) {
+      console.log("here");
+      next();
+    }
+  }
+});
+
+userRouter.get("/by_name/:nickname", async (req, res, next) => {
+  console.log(req.params);
   try {
-    const user = await User.findOne({ nickname: req.params.nickname });
+    const user = await basicUser.findOne({ nickname: req.params.nickname });
+    console.log("user is", user);
     if (!user) {
-      res
-        .status(404)
-        .send({ message: `User with ${req.params.nickname} is not found!` });
+      res.status(404).send({
+        message: `User with nickname: '${req.params.nickname}' was not found!`,
+      });
     } else res.send(user);
   } catch (error) {
     next();
@@ -50,5 +105,8 @@ userRouter.delete("/:_id", auth, async (req, res, next) => {
     next();
   }
 });
+
+// https://www.example.com/api/users/idfdfadsfasfads?page=2&limit=10#hash
+// protocol://top-level-domain.hostname.domain/pathname?query-string
 
 export default userRouter;
